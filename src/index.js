bccbd225cb54307716e5f354a5fa9342ea87d7e4
Alexandra Paskhaver <apaskhaver@alexandras-mbp.myfiosgateway.com>
2021-08-17 15:29:35 -0400

    Test everything works on localhost:3000 with a Hello World

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <h1>Hello World</h1>,
   document.getElementById('root')
 );
