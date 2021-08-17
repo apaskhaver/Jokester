b5b37f430354940bc367df4689b9ac434061cfa4
Alexandra Paskhaver <apaskhaver@alexandras-mbp.myfiosgateway.com>
2021-08-17 15:55:01 -0400

    Create Header, Footer, and App components; add functions displaying sample text and export statements

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+
+function App() {
+    return (
+    <div>
+        <Header />
+        <Footer />
+    </div>
+    );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright {new Date().getFullYear()}</p>
+        </footer>);
+}
+
+export default Footer;
