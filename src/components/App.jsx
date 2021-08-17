d2b2960959c6486acd357f2b381268998fbfc031
Alexandra Paskhaver <apaskhaver@alexandras-mbp.myfiosgateway.com>
2021-08-17 16:00:33 -0400

    Import and add Note

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./Header.jsx";
+import Note from "./Note.jsx";
 import Footer from "./Footer.jsx";
 
 function App() {
     return (
     <div>
         <Header />
+        <Note />
         <Footer />
     </div>
     );
