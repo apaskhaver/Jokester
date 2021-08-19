1c7f2c54a88ac935feb84eaee0b1d1ba8aadbd00
Alexandra Paskhaver <apaskhaver@alexandras-mbp.myfiosgateway.com>
2021-08-19 17:08:38 -0400

    Make createNote an arrow function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,15 @@ function App() {
     return (
         <div>
             <Header />
-            {notes.map(createNote)}
+            {notes.map((note) => 
+                (
+                    <Note 
+                        key={note.key}
+                        title={note.title}
+                        content={note.content}
+                    />
+                )
+            )}
             <Footer />
         </div>
     );
